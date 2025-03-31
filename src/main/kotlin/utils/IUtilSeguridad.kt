27bc77afa8b4b2ed21d04d563b1167316ef497da
José Antonio <115500375+JoseAntonioDiazBusati@users.noreply.github.com>
2025-04-01 00:34:48 +0200

    Update IUtilSeguridad.kt

--- src/main/kotlin/utils/IUtilSeguridad.kt
+++ src/main/kotlin/utils/IUtilSeguridad.kt
@@ -1,3 +1,5 @@
+package org.example.utils
+
 interface IUtilSeguridad {
     fun encriptarClave(clave: String, nivelSeguridad: Int = 12): String
     fun verificarClave(claveIngresada: String, hashAlmacenado: String): Boolean
